aced6f209a6a13d18ffa88616019181ff284076f
Damian Arroyo <damian.pk.89@gmail.com>
2022-02-04 08:42:01 -0600

    chore: project bootstrapping

---
+++ src/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace DC\Model;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
